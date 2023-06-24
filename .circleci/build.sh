3f5ffcf0e4c628cff32fae657c5728e4b23225dd
ᴍᴏᴄʜᴀ <118044457+xyz-mocha@users.noreply.github.com>
2023-06-24 15:18:37 +0530

    Happy

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -19,7 +19,7 @@ export KBUILD_BUILD_USER="xyz-mocha"
 # sticker plox
 function sticker() {
     curl -s -X POST "https://api.telegram.org/bot$token/sendSticker" \
-        -d sticker="CAACAgUAAxkBAAMQXvdgEdkCuvPzzQeXML3J6srMN4gAAvIAA3PMoVfqdoREJO6DahoE" \
+        -d sticker="CAACAgEAAxkBAAEnKnJfZOFzBnwC3cPwiirjZdgTMBMLRAACugEAAkVfBy-aN927wS5blhsE" \
         -d chat_id=$chat_id
 }
 # Send info plox channel
