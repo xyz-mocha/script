04c162f7ff8731f80dea8df9d1580deb09fbfc3f
ᴍᴏᴄʜᴀ <118044457+xyz-mocha@users.noreply.github.com>
2023-07-02 14:20:20 +0530

    And ?

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -53,10 +53,10 @@ function finerr() {
 function compile() {
     make O=out ARCH=arm64 tulip_defconfig
     make -j$(nproc --all) O=out \
-                          ARCH=arm64 \
-                          CC=clang \
-                          CLANG_TRIPLE=aarch64-linux-gnu- \
-                          CROSS_COMPILE_ARM32=arm-linux-gnueabi- \                   
+                    ARCH=arm64 \
+                    CC=clang \
+                    CROSS_COMPILE=aarch64-linux-gnu- \
+                    CROSS_COMPILE_ARM32=arm-linux-gnueabi-                   
 
     if ! [ -a "$IMAGE" ]; then
         finerr
