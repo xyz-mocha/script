0af546919b167effdada558d55749ed504e4ecf1
ᴍᴏᴄʜᴀ <118044457+xyz-mocha@users.noreply.github.com>
2023-08-05 13:23:40 +0530

    Mistake make you weak ;-;

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -5,6 +5,7 @@ cd kernel
 git clone --depth=1 https://github.com/sohamxda7/llvm-stable clang
 git clone https://github.com/sohamxda7/llvm-stable -b gcc64 --depth=1 gcc
 git clone https://github.com/sohamxda7/llvm-stable -b gcc32  --depth=1 gcc32
+git clone --depth=1 https://github.com/xyz-mocha/AnyKernel3 AnyKernel
 echo "Done"
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
 TANGGAL=$(date +"%F-%S")
